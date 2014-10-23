b8b610c24b78813a1c137320397fa7a1be5cdcff
Jeppe Dyrby <jeppe.dyrby@gmail.com>
2014-10-23 20:11:16 +0000

    added ui router to project

---
+++ src/js/config.js
@@ -0,0 +1,6 @@
+"use strict";
+
+export default angular.module('app.config', ['ui-router'])
+    .config(function () {
+       console.log('conf');
+    });
--- src/js/index.js
+++ src/js/index.js
@@ -3,11 +3,13 @@
 import movies from './movies/movies';
 import imRepeat from './im-repeat/im-repeat.directive';
 import imPath from './im-path/im-path.directive';
+import config from './config';
 
 var app =  angular.module('movies.app', [
     movies.name,
     imRepeat.name,
-    imPath.name
+    imPath.name,
+    config.name
   ])
   .run(function () {
     console.log('movies.app running');
